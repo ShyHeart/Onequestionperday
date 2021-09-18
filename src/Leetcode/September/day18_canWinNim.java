27fbbef95ff58aefa65c151a128eea9c8c5ac742
ShyHeart <18296998265@163.com>
2021-09-18 09:13:40 +0800

    292.Nim游戏

---
+++ src/Leetcode/September/day18_canWinNim.java
@@ -0,0 +1,14 @@
+package Leetcode.September;
+
+/**
+ * @作者 Heart
+ * @时间 2021/9/18 9:11
+ */
+
+//https://leetcode-cn.com/problems/nim-game/
+public class day18_canWinNim {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+}
