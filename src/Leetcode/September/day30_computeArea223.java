52ce2a250b19ec699d0d95bd817ce5aa0ef945bd
ShyHeart <18296998265@163.com>
2021-09-30 21:37:45 +0800

    223.矩形面积

---
+++ src/Leetcode/September/day30_computeArea223.java
@@ -0,0 +1,14 @@
+package Leetcode.September;
+
+/**
+ * @作者 Heart
+ * @时间 2021/9/30 21:32
+ */
+//https://leetcode-cn.com/problems/rectangle-area/
+public class day30_computeArea223 {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int x = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
+        int y = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
+        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - (x * y);
+    }
+}
