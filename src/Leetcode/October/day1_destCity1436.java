07a7fdda8b556badf429522e5909e74eb46b9e3e
ShyHeart <18296998265@163.com>
2021-10-01 12:14:05 +0800

    1436.旅行的终点站

---
+++ src/Leetcode/October/day1_destCity1436.java
@@ -0,0 +1,22 @@
+package Leetcode.October;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @作者 Heart
+ * @时间 2021/10/1 12:06
+ */
+//https://leetcode-cn.com/problems/destination-city/
+public class day1_destCity1436 {
+    public String destCity(List<List<String>> paths) {
+        Map<String, String> map = new HashMap<>();
+        for (List<String> p : paths)
+            map.put(p.get(0), p.get(1));
+        String ans = paths.get(0).get(0);
+        while (map.containsKey(ans)) ans = map.get(ans);
+        return ans;
+    }
+
+}
