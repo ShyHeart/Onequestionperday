d477e1e21a60aa22af3c2685fdac5b692a7fc08a
ShyHeart <18296998265@163.com>
2021-10-03 17:38:29 +0800

    166.分数到小数

---
+++ src/Leetcode/October/day3_166.java
@@ -0,0 +1,36 @@
+package Leetcode.October;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @作者 Heart
+ * @时间 2021/10/3 17:31
+ */
+//https://leetcode-cn.com/problems/fraction-to-recurring-decimal/
+public class day3_166 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        //转long计算，防止溢出
+        long a = numerator, b = denominator;
+        if (a % b == 0) return String.valueOf(a / b);
+        StringBuilder sb = new StringBuilder();
+        if (a * b < 0) sb.append('-');
+        a = Math.abs(a);
+        b = Math.abs(b);
+        sb.append(String.valueOf(a / b) + ".");
+        a %= b;
+        Map<Long, Integer> map = new HashMap<>();
+        while (a != 0) {
+            map.put(a, sb.length());
+            a *= 10;
+            sb.append(a / b);
+            a %= b;
+            if (map.containsKey(a)) {
+                int u = map.get(a);
+                return String.format("%s(%s)", sb.substring(0, u), sb.substring(u));
+
+            }
+        }
+        return sb.toString();
+    }
+}
