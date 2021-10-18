6962adaddc29b9cdebec3cf3427d45ec4c3c2c6f
ShyHeart <18296998265@163.com>
2021-10-18 22:12:30 +0800

    476.数字的补数

---
+++ src/Leetcode/October/day18_476.java
@@ -0,0 +1,20 @@
+package Leetcode.October;
+
+//https://leetcode-cn.com/problems/number-complement/
+public class day18_476 {
+    public int findComplement(int num) {
+        int s = -1;
+        for (int i = 31; i >= 0; i--) {
+            if (((num >> i) & 1) != 0) {
+                s = i;
+                break;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < s; i++) {
+            if (((num >> i) & 1) == 0) ans |= (1 << i);
+        }
+        return ans;
+    }
+
+}
