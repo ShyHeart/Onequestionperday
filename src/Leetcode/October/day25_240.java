7d162379bc566836f5189686c9f2f709d5f215ef
ShyHeart <18296998265@163.com>
2021-10-25 09:47:48 +0800

    240.搜索矩阵II

---
+++ src/Leetcode/October/day25_240.java
@@ -0,0 +1,18 @@
+package Leetcode.October;
+
+public class day25_240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (matrix[i][mid] <= target) l = mid;
+                else r = mid - 1;
+            }
+            if (matrix[i][r] == target) return true;
+        }
+        return false;
+    }
+
+}
